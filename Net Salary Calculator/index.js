3f5b4a4be8fb87a16aa60218fc16d111be5fc980
Lisa <wamaithak05@gmail.com>
2024-03-24 16:10:10 +0300

    netPay updated

--- Net Salary Calculator/index.js
+++ Net Salary Calculator/index.js
@@ -6,5 +6,7 @@ function employeeSalary() {
     //NSSF is 0.004 of the netPay
     //PAYE is 0.3 of the netPay
 
-
+    NHIF = 0.017 * netPay;
+    NSSF = 0.004 * netPay;
+    PAYE = 0.3 * netPay;
 }

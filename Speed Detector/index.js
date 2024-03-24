b3e7c37002948d88f3eeef303887597eb832cbf7
Lisa <wamaithak05@gmail.com>
2024-03-24 15:39:03 +0300

    speed limit added

--- Speed Detector/index.js
+++ Speed Detector/index.js
@@ -1,4 +1,8 @@
 function checkSpeed(speed) {
-    const speedLimit = 70
-    const kmPerPoint = 5
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if(speed < speedLimit + kmPerPoint) {
+        console.log("Ok");
+    }
 }

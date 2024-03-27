3db12b59353475e02f81cc34e0cbc6d27670379c
Lisa <wamaithak05@gmail.com>
2024-03-28 00:51:38 +0300

    speed-detector.js comments added

--- Speed Detector/index.js
+++ Speed Detector/index.js
@@ -1,3 +1,4 @@
+// This function calculates the deduction points based on the vechile's speed and showcases the corresponding points.
 function checkSpeed(speed) {
  
     let carSpeed = document.querySelector("#carSpeed").ariaValueMax;
@@ -5,7 +6,7 @@ function checkSpeed(speed) {
     console.log(carSpeed);
 
     let result = "OK";
-
+// Determine the message displayed based on the calculated deduction points
     if (carSpeed > 70) {
 
         let demeritPoints = (carSpeed - 70) / 5;
